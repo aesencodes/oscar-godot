539e02340f0ea10c33b9f2aa22a86fb7ca91abf0
aesencodes <adjiesuryanugraha1@gmail.com>
2023-11-16 22:48:11 +0700

    Added main menu

---
+++ main-menu/main_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_play_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_2_pressed():
+	get_tree().quit()
